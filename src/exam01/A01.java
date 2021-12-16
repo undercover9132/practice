700bb03323268dad285940d250aab0c0223ef566
undercover9132 <ed5160@naver.com>
2021-12-17 00:36:09 +0900

    commit

---
+++ src/exam01/A01.java
@@ -0,0 +1,20 @@
+package exam01;
+
+
+public class A01 {
+	public static void main(String[] args) {
+		//print : 한줄로 출력되는 형태
+		System.out.print("hello");
+		System.out.print("world");
+		//println : 실행된 후 한줄을 띄우는 출력형태
+		System.out.println("hello");
+		System.out.println("world");
+		
+		// <- 한줄 주석 
+		 
+		/*
+		 *  <- 여러줄 주석 
+		 */
+	}
+}
+ 
